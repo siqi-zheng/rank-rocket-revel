eb6a6057b5ebd3ee68a1917d5350f8a7fb428738
Siqi Zheng <64695253+siqi-zheng@users.noreply.github.com>
2026-03-24 19:19:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/rank-rocket-revel/", // Add this line for GitHub Pages
+  base: "/", // Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
